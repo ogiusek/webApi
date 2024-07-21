49487c73a1075fd474e9966f78dabbb9146cefe4
ogius <kowalewski.olgierd@gmail.com>
2024-07-21 11:47:30 +0200

    added mapper as service

---
+++ src/global/services/mapper/implementation.cs
@@ -0,0 +1,25 @@
+namespace WebApi.Global.Services.Mapper;
+
+using WebApi.Global.Attributes;
+using System.Reflection;
+
+[Service<IMapper>]
+public class Mapper : IMapper{
+    private readonly IEnumerable<IMapBase> _maps;
+
+    public Mapper(){
+        _maps = Assembly
+            .GetExecutingAssembly()
+            .GetTypes()
+            .Where(t => t.GetInterfaces().Where(i => i.GetInterfaces().Where(i => i == typeof(IMapBase)).Count() != 0).Count() != 0)
+            .Select(t => (IMapBase)Activator.CreateInstance(t)!);
+    }
+
+    public TTo? Map<TFrom, TTo>(TFrom from) where TTo : class where TFrom : class {
+        IEnumerable<IMap<TFrom, TTo>> maps = _maps
+            .Where(m => m is IMap<TFrom, TTo>)
+            .Select(m => (IMap<TFrom, TTo>)m);
+        if(maps.Count() == 0) return null;
+        return maps.First().Map(from);
+    }
+}
---
+++ src/global/services/mapper/interface.cs
@@ -0,0 +1,16 @@
+namespace WebApi.Global.Services.Mapper;
+
+public interface IMapper {
+    TTo? Map<TFrom, TTo>(TFrom from) where TTo : class where TFrom : class;
+};
+
+// <summary>
+//  !IMPORTANT!
+//  using this interface can destroy the mapper
+//  this interface should only be used by IMap interface
+// </summary>
+public interface IMapBase {};
+
+public interface IMap<TFrom, TTo> : IMapBase {
+    TTo Map(TFrom from);
+}
