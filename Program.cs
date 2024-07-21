b52eda424519a0ffb7ede1bcbf3a30d57eb572e4
ogius <kowalewski.olgierd@gmail.com>
2024-07-21 11:44:58 +0200

    created program.cs and created attributes

---
+++ Program.cs
@@ -0,0 +1,43 @@
+namespace WebApi;
+
+// asp net core
+// jwt
+// identity - for external authentication
+
+// EntityFramework
+// linq (pretty much js array functions)
+// xunit for testing
+
+using Global.Attributes;
+
+static class Program{
+    public static void Main(string[] args){
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.ConfigureServices();
+
+        var app = builder.Build();
+        app.ConfigureApp();
+
+        app.Run();
+    }
+
+    public static void ConfigureServices(this IServiceCollection services){
+        services.Addservices();
+        services.AddControllers(options => {
+            options.AddFilters();
+        });
+    }
+
+    public static void ConfigureApp(this WebApplication app){
+        app.MapControllers();
+        app.AddMiddlewares();
+
+        app.UseAuthorization();
+        app.UseRouting();
+        // app.UseHttpsRedirection();
+
+        if (!app.Environment.IsDevelopment())
+            return;
+        // development
+    }
+}
